df992c2bc4084fecc39468dd0f37baa56de27dba
amaljenni <jenni.amal@yahoo.fr>
2019-08-09 10:54:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,32 +4,38 @@ import './App.css';
 import Photos from "./Photos"
 import Text from "./Text"
 import Link from "./Link"
+import Lifecycle from "./Lifecycle" 
 
 
 function App() {
-  return (
-    document.body.style.backgroundColor = "rgb(219,112,147)",
-    <div className="App" >
-      {/* <div className="row-md-12">
-        <div className="col-md-3">
+  
+    return (
+      document.body.style.backgroundColor = "rgb(219,112,147)",
+      <div className="App" >
+        <Lifecycle/>
+        {/* <div className="row-md-12">
+          <div className="col-md-3">
+  
+          </div>
+          <div className="col-md-3"> */}
+            {/* <Photos/> */}
+          {/* </div>
+          <div className="col-md-3"> */}
+            {/* <Text/> */}
+          {/* </div>
+          <div className="col-md-3">
+  
+          </div>
+        </div> */}
+        
+        
+       
+       
+            {/* <Link/> */}
+      </div>
+    )
+  }
+  
 
-        </div>
-        <div className="col-md-3"> */}
-          <Photos/>
-        {/* </div>
-        <div className="col-md-3"> */}
-          <Text/>
-        {/* </div>
-        <div className="col-md-3">
-
-        </div>
-      </div> */}
-      
-     
-     
-          <Link/>
-    </div>
-  );
-}
 
 export default App;
